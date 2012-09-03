4c7d5730770e57c1e59fb3cae22127c183635e92
boxme <boxxme_cuba@hotmail.com>
2012-09-03 23:18:16 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '5c453db5a84167f499fdf8f65fb4433c226e96ad9c0bb4585d93fce11d09bbaed818f49c6950a588c2b3f84b9bd24e8ab0a3abe519859fc0cbdf180deb2e9968'
